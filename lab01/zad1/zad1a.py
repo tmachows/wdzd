ad5a004b513fcc5e4cc8856d68df782e27a0fe32
tmachows <tmachows@gmail.com>
2016-10-08 15:38:42 +0200

    zad1b + 1a extension

--- lab01/zad1/zad1a.py
+++ lab01/zad1/zad1a.py
@@ -8,38 +8,55 @@ plot_ns = [2, 10, 50, 100, 150, 200]
 
 
 def generate_point_pair(n):
-    point1 = np.random.random_sample((n,))
-    point2 = np.random.random_sample((n,))
-    return (point1, point2)
+	point1 = np.random.random_sample((n,))
+	point2 = np.random.random_sample((n,))
+	return (point1, point2)
 
 
 def count_distance(point_pair):
-    return distance.euclidean(point_pair[0], point_pair[1])
+	return distance.euclidean(point_pair[0], point_pair[1])
+
+
+def create_chart(n, distances):
+	hist, bins = np.histogram(distances, bins=20)
+	width = 0.2 * (bins[1] - bins[0])
+	center = (bins[:-1] + bins[1:]) / 2
+	plt.hist(distances, bins)
+	plt.savefig('a_dim_'+str(n)+'.png', dpi=96)
+	plt.clf()
 
 
 def main():
-    for n in range(2, 101):
-        distances = []
+	means = []
+	variances = []
 
-        for i in range(1, 1001):
-            point_pair = generate_point_pair(n)
-            distance = count_distance(point_pair)
-            distances.append(distance)
+	for n in range(2, 201):
+		distances = []
 
-        mean = np.mean(distances)
-        var = np.var(distances)
+		for i in range(1, 1001):
+			point_pair = generate_point_pair(n)
+			distance = count_distance(point_pair)
+			distances.append(distance)
 
-        # print "Mean: ", mean
-        # print "Variance: ", var
+		mean = np.mean(distances)
+		var = np.var(distances)
 
-        if n in plot_ns:
-            hist, bins = np.histogram(distances, bins=20)
-            width = 0.2 * (bins[1] - bins[0])
-            center = (bins[:-1] + bins[1:]) / 2
-            plt.hist(distances, bins)
-            plt.savefig('file'+str(n)+'.png', dpi=96)
-            plt.clf()
+		means.append(mean)
+		variances.append(var)
 
+		# print "Mean: ", mean
+		# print "Variance: ", var
+
+		if n in plot_ns:
+			create_chart(n, distances)
+
+	plt.plot(range(2, 201), means)
+	plt.savefig('a_means.png', dpi=96)
+	plt.clf()
+
+	plt.plot(range(2, 201), variances)
+	plt.savefig('a_variances.png', dpi=96)
+	plt.clf()
 
 if __name__ == "__main__":
-    main()
+	main()
